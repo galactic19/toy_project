76d3f00b3578521e194f30f097038e4369193b9d
Beoms <galactic19@naver.com>
2022-07-13 15:51:30 +0900

    Category, Product  model 생성

--- category/models.py
+++ category/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, unique=True, allow_unicode=True)
+    description = models.CharField(max_length=200, blank=True)
+    meta_description = models.CharField(max_length=120, blank=True)
+    image = models.ImageField(upload_to='category/%Y/%m/%d', blank=True)
+    parent = models.ForeignKey('self', null=True, default=None)
+    # sequence = models. 별도의 메서드를 생성해야 할듯.
+    
+    class Meta:
+        ordering = ['name', 'parent']
+        verbose_name_plural = '카테고리'
+    
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse('category:category_list', kwargs={"slug":self.slug})
--- product/models.py
+++ product/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Thumb(models.Model):
+    ''' 썸네일 이미지를 관리 '''
+    front_image = models.ImageField(upload_to='thumb/%Y/%m/%d', null=True, default='none.png')
+    left_image = models.ImageField(upload_to='thumb/%Y/%m/%d', blank=True)
+    back_image = models.ImageField(upload_to='thumb/%Y/%m/%d', blank=True)
+    right_image = models.ImageField(upload_to='thumb/%Y/%m/%d', blank=True)
+
+    class Meta:
+        verbose_name_plural = '썸네일 이미지'
+    
+    def __str__(self):
+        return self.front_image.url
+        
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, unique=True, db_index=True)
+    model_name = models.CharField(max_length=100, unique=True, db_index=True)
+    slug = models.SlugField(max_length=100, unique=True, allow_unicode=True)
+    price = models.PositiveSmallIntegerField()
+    thumbnail = models.ForeignKey(Thumb, on_delete=models.CASCADE)
+    
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_at', '-updated_at', 'name']
+    
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse("product:product_detail", kwargs={"slug": self.slug})
+    
